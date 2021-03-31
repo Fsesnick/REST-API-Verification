78420fed88f5bcf7172d1ed8d9b58f2cfe55a699
Fsesnick <61622322+Fsesnick@users.noreply.github.com>
2021-03-31 15:06:47 -0300

    Validators and Custom Error Messages

--- models/user.js
+++ models/user.js
@@ -6,19 +6,51 @@ module.exports = (sequelize) => {
   User.init({
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A name is required'
+        },
+        notEmpty: {
+          msg: 'Please provide a name'
+        }
+      }
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false 
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'An email is required'
+        },
+        isEmail: {
+          msg: 'Please provide a valid email address'
+        }
+      }
     },
     birthday: {
       type: DataTypes.DATEONLY,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A birthday is required'
+        },
+        notEmpty: {
+          msg: 'Please provide a birthday'
+        }
+      }
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A password is required'
+        },
+        notEmpty: {
+          msg: 'Please provide a password'
+        }
+      }
     }
   }, { sequelize });
 
