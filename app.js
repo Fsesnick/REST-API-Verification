e0a1a7e9bce6c5b33b874914e13ae3d670f364c5
Fsesnick <61622322+Fsesnick@users.noreply.github.com>
2021-03-31 22:50:02 -0300

    REST API Authentication with Express init

--- app.js
+++ app.js
@@ -3,27 +3,28 @@
 const express = require('express');
 const morgan = require('morgan');
 const routes = require('./routes');
+const { sequelize } = require('./models');
 
-// Crie um app Express.
+// Create the Express app.
 const app = express();
 
-// Configuracao request body JSON parsing.
+// Setup request body JSON parsing.
 app.use(express.json());
 
-//Setup Morgan que nos dá o registro de requisições HTTP. 
+// Setup morgan which gives us HTTP request logging.
 app.use(morgan('dev'));
 
-// Configure uma saudação amigável para a rota raiz. 
+// Setup a friendly greeting for the root route.
 app.get('/', (req, res) => {
   res.json({
-    message: 'Bem-vindo ao  projeto validação de API REST com Express!',
+    message: 'Welcome to the REST API & Sequelize model validation project!',
   });
 });
 
-// Adicionar routes.
+// Add routes.
 app.use('/api', routes);
 
-// enviar 404 se nenhuma outra rota é correspondente
+// Send 404 if no other route matched.
 app.use((req, res) => {
   res.status(404).json({
     message: 'Route Not Found',
@@ -43,7 +44,20 @@ app.use((err, req, res, next) => {
 // Set our port.
 app.set('port', process.env.PORT || 5000);
 
-// Start listening on our port.
-const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
-});
+// Test the database connection.
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
+// Sequelize model synchronization, then start listening on our port.
+sequelize.sync()
+  .then( () => {
+    const server = app.listen(app.get('port'), () => {
+      console.log(`Express server is listening on port ${server.address().port}`);
+    });
+  });
